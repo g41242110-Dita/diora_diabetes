4cfb2a2d60a2e498ea48e96d1f1402d24b0c9c67
g41242110-Dita <g41242110@student.polije.ac.id>
2026-09-18 11:07:52 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,60 +4,60 @@ import 'firebase_options.dart';
 import 'onboarding_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-  runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HalamanUtama(),
-    );
-  }
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HalamanUtama(),
+    );
+  }
 }
 
 class HalamanUtama extends StatefulWidget {
-  const HalamanUtama({super.key});
+  const HalamanUtama({super.key});
 
-  @override
-  State<HalamanUtama> createState() => _HalamanUtamaState();
+  @override
+  State<HalamanUtama> createState() => _HalamanUtamaState();
 }
 
 class _HalamanUtamaState extends State<HalamanUtama> {
-  @override
-  void initState() {
-    super.initState();
+  @override
+  void initState() {
+    super.initState();
 
-    Future.delayed(const Duration(seconds: 3), () {
-      if (!mounted) return;
+    Future.delayed(const Duration(seconds: 3), () {
+      if (!mounted) return;
 
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const OnboardingPage(),
-        ),
-      );
-    });
-  }
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnboardingPage(),
+        ),
+      );
+    });
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox.expand(
-        child: Image.asset(
-          'assets/HAL UTAMA.png',
-          fit: BoxFit.cover,
-        ),
-      ),
-    );
-  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Image.asset(
+          'assets/HAL UTAMA.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
 }
